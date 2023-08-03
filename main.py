5381ac8f838360f6da25ca045d3ed74f613e5623
AlOnline20 <110356237+AlOnline20@users.noreply.github.com>
2023-08-03 02:21:07 +0000

    ok

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-print("dani devito")
+# sleep
+
+from time import sleep
+
+# yes or no function
+def yes_no(question):
+
+  while True:
+    response = input(question).lower()
+
+    if response == "yes" or response == "y":
+      return "yes"
+
+    elif response == "no" or response == "n":
+      return "no"
+
+    else:
+      print("please enter yes or no")
+
+# main routine down below
+
+want_instructions = yes_no("Do you want to read the instructions? :")
+sleep(1)
+
+if want_instructions == "yes":
+  print("")
+  print("")
+  print("")
+  print("")
+  sleep(3)
+
+print("Now to begin")
+sleep(2)
+print()
